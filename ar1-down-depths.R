97b1f49f570b32ad6762c7bb512c88694e22f6a9
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-07-08 17:00:51 +1000

    Ar1 process down depths for non gridded data.

---
+++ ar1-down-depths.R
@@ -0,0 +1,74 @@
+library(asreml)
+library(mgcv)
+#--------------------------- AR1 DOWN DEPTHS -------------------------------#
+
+n.station <- 150 #specify number of stations to run
+mu <- 50
+sd <- 40
+noise.sd <- 0.2 #noise sd for ar process
+stn.sd   <- 0.65 #sd for random station effect
+
+mult <- 1e3
+z <- seq(0, 250, 5) #explanatory variable (depth)
+rho <- mult*dnorm(z, mu, sd)/(pnorm(max(z), mu, sd) - pnorm(min(z), mu, sd))
+stn.re <- rnorm(n.station, mean = 0, sd = stn.sd) #station specific random effect
+phi.true <- 0.6
+
+#calculate obs, the observed data, and log(obs)
+l.obs <- NULL
+
+#ar process down z (same for each station)
+for (i in c(1:n.station)) {
+  l.temp <- log(rho) + arima.sim(n = length(rho), model = list(ar = phi.true), sd = noise.sd, 
+                                 innov=rnorm(length(rho), mean=0, sd=0.2)) +  rep(stn.re[i],length(rho)) 
+  l.obs <- c(l.obs,l.temp)
+}
+obs <- exp(l.obs)
+
+#data frame
+glm.spl <- data.frame(obs, l.obs, rep(z, n.station), rep(c(1:n.station), 1, each = length(z)), as.factor(z))
+names(glm.spl) <- c("obs", "l.obs", "z", "stn", "z.fact")
+glm.spl$stn <- as.factor(glm.spl$stn)
+
+glm.spl$z.fact <- as.factor(as.integer(glm.spl$z.fact))
+
+#---------------------------------- asreml -------------------------------------#
+
+asreml.fit <- asreml(fixed = l.obs ~ z, random =~ spl(z) + stn, data = glm.spl, 
+                     splinepoints = list(z = seq(0, 250, 25)), rcov =~ stn:ar1(z.fact))
+
+summary(asreml.fit)
+summary(asreml.fit)$varcomp[2,2]^0.5
+summary(asreml.fit)$varcomp[3,2]^0.5
+summary(asreml.fit)$varcomp[4,2]
+
+#variogram
+v1 <- Variogram(gam.fit$lme, form =~ z.int|stn, breaks=seq(0,15,1),  robust = TRUE)
+plot(v1$dist, v1$variog)
+lines(y = (1 - phi^(seq(0, 15,1))), x = seq(0, 15,1), lwd = 2)
+
+
+#---------------------------------- gamm ---------------------------------------#
+
+#make variable for time series distance rather than using depths
+glm.spl$z.int <- as.integer(glm.spl$z.fact)
+
+gam.fit <- gamm(l.obs ~ s(z),  random=list(stn =~1), data = glm.spl, 
+                correlation = corAR1(phi, form =~ z.int|stn))
+
+summary(gam.fit$lme)
+summary(gam.fit$gam)
+intervals <- intervals(gam.fit$lme)
+phi <- intervals$corStruct[1,2]
+
+#variogram
+v1 <- Variogram(gam.fit$lme, form =~ z.int|stn, breaks=seq(0,15,1),  robust = TRUE)
+plot(v1$dist, v1$variog)
+lines(y = (1 - phi^(seq(0, 15,1))), x = seq(0, 15,1), lwd = 2)
+
+
+
+
+
+
+
