82c244b3be0b34384b050d7f2ad74c7061c8c05b
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-07-16 11:55:32 +1000

    Now working well. Added extra 0.5 noise into the autocorrelation process now ar1 and agau are all estimated well and error sd is estimated at 0.5 its actual value.

--- ar1_z_agau_xy_combined_process.R
+++ ar1_z_agau_xy_combined_process.R
@@ -6,10 +6,10 @@ n.station <- 100 #specify number of stations to run
 mu <- 50
 sd <- 40
 noise.sd <- 0.2 #noise sd for ar process
-stn.sd   <- 0.65 #sd for random station effect
+stn.sd   <- 0.05 #sd for random station effect
 phi.true <- 0.55 #autocorrelation (ar1) down depths
-x.phi <- 0.6 #autocorrelation across x
-y.phi <- 0.3 #autocorrelation across y
+x.phi <- 0.45 #autocorrelation across x
+y.phi <- 0.45 #autocorrelation across y
 
 mult <- 1e3
 z <- seq(0, 250, 5) #explanatory variable (depth)
@@ -43,11 +43,14 @@ for (i in 1:max(y)) {
 
 #combined ar component
 t.cor <- rep(0, length(r.noise))
-for (i in 1:max(y)) {
-  for (k in 2:length(z)) {  
+for (k in 2:length(z)) {  
+  for (i in 1:max(y)) {
     for (j in 1:max(x)) {
       w <- which(y == i & z.int == k & x == j)
-      t.cor[w] <- t.cor[y == i & z.int == (k - 1) & x == j]*phi.true + sum((matrix(rep(cor.ij[c(1:max(x)), j], max(y)), ncol = max(x), byrow = T) * matrix(rep(cor.ij.y[c(1:max(y)), i], max(x)), ncol = max(x)))*matrix(r.noise[z.int == k], ncol = max(x), byrow = T))
+      x.cor <- matrix(rep(cor.ij[, j], max(y)), ncol = max(x), byrow = T) #correlation matrix 
+      y.cor <- matrix(rep(cor.ij.y[, i], max(x)), ncol = max(x))
+      z.data <- matrix(r.noise[z.int == k], ncol = max(x))
+      t.cor[w] <- t.cor[y == i & z.int == (k - 1) & x == j]*phi.true + sum((x.cor*y.cor)*z.data) + rnorm(1, 0, noise.sd/2)
     }
   }
 }
@@ -63,26 +66,20 @@ names(glm.spl) <- c("obs", "l.obs", "z", "stn", "x", "y")
 glm.spl$z.fact <- as.factor(as.integer(glm.spl$z))
 glm.spl$x.fact <- as.factor(as.integer(glm.spl$x))
 glm.spl$y.fact <- as.factor(as.integer(glm.spl$y))
-glm.spl <- glm.spl[order(glm.spl$z, glm.spl$y), ] #sort by order of rcov structure
+glm.spl <- glm.spl[order(glm.spl$z, glm.spl$x), ] #sort by order of rcov structure
 
 
 #---------------------------------- asreml -------------------------------------#
 
 asreml.fit <- asreml(fixed = l.obs ~ z, random =~ spl(z) + stn, data = glm.spl, 
-                     splinepoints = list(z = seq(0, 250, 25)), rcov =~ ar1(z.fact):agau(x.fact, y.fact))
+                     splinepoints = list(z = seq(0, 250, 25)), rcov=~ ar1(z.fact):agau(x.fact, y.fact), maxIter = 50)
 summary(asreml.fit)
 
 
-vals <- matrix(c(stn.sd, noise.sd, phi.true, x.phi, x.phi, round(summary(asreml.fit)$varcomp[2,2]^0.5, 2), round(summary(asreml.fit)$varcomp[3,2]^0.5, 2), round(summary(asreml.fit)$varcomp[4,2], 2), round(summary(asreml.fit)$varcomp[5,2], 2), round(summary(asreml.fit)$varcomp[6,2], 2)), ncol = 2)
+vals <- matrix(c(stn.sd, 1.5*noise.sd, phi.true, x.phi, y.phi, round(summary(asreml.fit)$varcomp[2,2]^0.5, 2), round(summary(asreml.fit)$varcomp[3,2]^0.5, 2), round(summary(asreml.fit)$varcomp[4,2], 2), round(summary(asreml.fit)$varcomp[5,2], 2), round(summary(asreml.fit)$varcomp[6,2], 2)), ncol = 2)
 colnames(vals) <- c("true", "fitted")
-rownames(vals) <- c("stn", "noise", "z ar1", "y agau", "x agau")
-vals
-
-
-
-
-
-
+rownames(vals) <- c("stn", "noise", "z ar1", "x agau", "y agau")
+vals 
 
 
 
