c88e6be66712c41d9dd21ff3a78d03e7f5abbc84
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-09-02 11:14:33 +1000

    Changed correlation function so that error is estimated for the top level of the data. Previously depth = 1 was always 0!

--- ar1_z_agau_xy.R
+++ ar1_z_agau_xy.R
@@ -1,21 +1,21 @@
-#simulates a regular grid with anisotropic exponential correlation in the x and y directions, 
-#ar1 correlation in the z direction
+#simulates a regular grid with anisotropic gaussian correlation in the x and y directions, 
+#and ar1 correlation in the z direction
 #author: Lisa-Marie Harrison
-#date: 14/08/2014
+#date: 02/09/2014
 library(asreml)
 library(mgcv)
 library(fields)
 
 n.station <- 100 #specify number of stations to run
-mu <- 50
-sd <- 40
 noise.sd <- 0.2 #noise sd for ar process
 stn.sd   <- 0.05 #sd for random station effect
-z.phi <- 0.4 #autocorrelation (ar1) down depths
-x.phi <- 0.2 #exponential autocorrelation across xy
-y.phi <- 0.3
+z.phi <- 0.4 #ar1 down depths
+x.phi <- 0.2 #x component of agau correlation
+y.phi <- 0.3 #y component of agau correlation
 
 mult <- 1e3
+mu <- 50
+sd <- 40
 z <- seq(0, 250, 5) #explanatory variable (depth)
 z.int <- rep(c(1:length(z)), n.station) #explanatory variable (depth)
 
@@ -30,17 +30,16 @@ y <- rep(sort(rep(seq(1:sqrt(n.station)), length(rho))), sqrt(n.station))
 #random noise matrix
 r.noise <- rnorm(length(x), 0, noise.sd)
 
-cols <- 10
-rows <- 10
-#create dataframe (xyz format)
-X <- rep(1:10, each = rows)
-Y <- rep(1:10, cols)
+cols <- sqrt(n.station)
+rows <- sqrt(n.station)
+#create coordinates data frame (xyz format)
+X <- rep(1:sqrt(n.station), each = rows)
+Y <- rep(1:sqrt(n.station), cols)
+coords <- data.frame(X, Y)
 
 # create a spatial autocorrelation signature
-# coordinate list
-coords <- data.frame(X, Y)
-# distance matrix
 
+# distance matrix
 loc <- matrix(c(1:100), ncol = 10, byrow = T)
 dist_x <- matrix(0, ncol = 100, nrow = 100)
 for (i in 1:100) {
@@ -60,21 +59,30 @@ for (i in 1:100) {
   }
 }
 
-# create a correlation structure (exponential)
+dist_x <- as.matrix(as.dist(dist_x, diag = FALSE, upper = FALSE))
+dist_y <- as.matrix(as.dist(dist_y, diag = FALSE, upper = FALSE))
+
+# create the correlation structure
 omega1 <- (x.phi^dist_x)*(y.phi^dist_y)
-# calculate correlation weights, and invert weights matrix
+
+# calculate weights and invert weights matrix
 weights <- chol(solve(omega1))
 weights_inv <- solve(weights)
 
 #combined correlated error component
 t.cor <- rep(0, length(r.noise))
-for (k in 2:length(z)) {
+for (k in 1:length(z)) {
   z.data <- matrix(r.noise[z.int == k], ncol = max(x))
   xy_error <- matrix(weights_inv %*% matrix(z.data, ncol = 1, byrow = T), ncol = 10, byrow = T)
   for (i in 1:max(y)) {
     for (j in 1:max(x)) {
       w <- which(y == i & z.int == k & x == j)
-      t.cor[w] <- t.cor[y == i & z.int == (k - 1) & x == j]*z.phi + xy_error[i, j]
+      #only use z.phi for depths > 1
+      if (k != 1) {
+        t.cor[w] <- t.cor[y == i & z.int == (k - 1) & x == j]*z.phi + xy_error[i, j]  
+      } else {
+        t.cor[w] <- xy_error[i, j]        
+      }
     }
   }
 }
