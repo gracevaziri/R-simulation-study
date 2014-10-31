1b459d06c6fa60fc75d089a944888f874f8cc892
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2014-10-31 17:03:18 +1100

    Added prediction function to predict fluoroescence at stations that weren't fitted in the model to test extrapolation.

--- broke_simulation_test.R
+++ broke_simulation_test.R
@@ -7,13 +7,14 @@ setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/Mixed models/Data")
 dat.cut <- read.csv(file = "rstnCTD.csv", header= T)
 library(asreml)
 library(nlme)
+library(lattice)
+library(mgcv)
 
 #remove null values
 dat.cut$sal[dat.cut$sal == -9] <- NA
 dat.cut$temp[dat.cut$temp == -9] <- NA
 dat.cut$par[dat.cut$par == -9] <- NA
 dat.cut$fluoro[dat.cut$fluoro == -9] <- NA
-
 
 #get latitude and longitude for each station
 n.station <- length(unique(dat.cut$stn))
@@ -25,8 +26,7 @@ deg2rad <- function(deg) {
   return(deg*pi/180)
 }
 
-# Calculates the distance between two points with radian latitude/longitude using 
-# Haversine formula (hf)
+#Calculates the distance between two points with radian latitude/longitude using Haversine formula (hf)
 gcd.hf <- function(lat1, long1, lat2, long2) {
   R <- 6371 # Earth mean radius [km]
   delta.long <- (long2 - long1)
@@ -56,8 +56,6 @@ x <- dist_x[1, ]
 y <- dist_y[1, ]
 
 
-
-
 #data frame
 glm.spl <- data.frame(log(dat.cut$fluoro - min(na.omit(dat.cut$fluoro))), dat.cut$profile.depth, as.factor(dat.cut$stn), rep(x, 1, each = length(unique(dat.cut$profile.depth))), rep(y, 1, each = length(unique(dat.cut$profile.depth))), dat.cut$temp, dat.cut$par, dat.cut$sal, dat.cut$oxy, dat.cut$ice, as.factor(dat.cut$water.mass))
 names(glm.spl) <- c("l.obs", "z", "stn", "x", "y", "temp", "par", "sal", "oxy", "ice", "wm")
@@ -78,26 +76,74 @@ glm.spl$ice  <- scale(glm.spl$ice)
 #------------------------------- FIT ASREML MODELS -----------------------------------#
 
 #fit asreml model
-asreml.fit <- asreml(fixed = l.obs ~ z + par  + temp:wm, random =~ spl(z, 10) + spl(par, 10) + 
-                       + spl(temp, 10):wm + stn, 
+asreml.fit <- asreml(fixed = l.obs ~ z + par + temp:wm + ice , random =~ spl(z, 10) + spl(par, 10) + 
+                        spl(temp, 10):wm +  spl(ice, 10) + stn, 
                      data = glm.spl, rcov=~ ar1(z.fact):agau(x.fact, y.fact),
                      na.method.X = "include", workspace = 50000000)
 asreml.fit <- update(asreml.fit)
 summary(asreml.fit)
 
 
-#plot fitted against observed
-unique(glm.spl$stn)
-s <-  14  
-plot(glm.spl$l.obs[glm.spl$stn == s])
-points(fitted(asreml.fit)[glm.spl$stn == s], col = "red")
 
+#plot fitted against observed for all stations
+lat.plot <- xyplot(glm.spl$l.obs + fitted(asreml.fit) ~ glm.spl$z | glm.spl$stn, 
+                   outer = FALSE, type = "l", xlab = "depth (m)", ylab = "l.fluoro")
+update(lat.plot, par.settings = simpleTheme(lwd = c(2, 1), col = c("dodgerblue", "red")))
 
 #fit the same asreml model but without the correlation structure
-fit <- asreml(fixed = l.obs ~ z + par  + temp:wm, random =~ spl(z, 10) + spl(par, 10) + 
-                       + spl(temp, 10):wm + stn, 
-                     data = glm.spl,
-                     na.method.X = "include", workspace = 50000000)
+fit <- asreml(fixed = l.obs ~ z + par + temp:wm + ice , random =~ spl(z, 10) + spl(par, 10) + 
+                       spl(temp, 10):wm +  spl(ice, 10) + stn, 
+                     data = glm.spl, na.method.X = "include", workspace = 50000000)
+
+b.int <- fit$coefficients$fixed["(Intercept)"]
+temp <- fit$coefficients$fixed[c(1:4)]
+ice <- fit$coefficients$fixed["ice"]
+par <- fit$coefficients$fixed["par"]
+z <- fit$coefficients$fixed["z"]
+wm <- as.numeric(glm.spl$wm) #water mass indicator variable
+
+n <- fit$coefficients$random
+z_spl <- n[which(substr(names(n), start = 1, stop = 5) %in% "spl(z")]
+par_spl <- n[which(substr(names(n), start = 1, stop = 5) %in% "spl(p")]
+ice_spl <- n[which(substr(names(n), start = 1, stop = 5) %in% "spl(i")]
+temp_spl <- n[which(substr(names(n), start = 1, stop = 5) %in% "spl(t")]
+
+pred.fixed <- b.int + z*depth + ice*i + par*light + temp[wm]*temp
+pred.random <- depth*1
+
+#----------------------- PREDICT VALUES AT OTHER STATIONS ---------------------#
+
+#predict at each point using parameter values for other stations to test extrapolation
+#using depth, par, temperature, ice and watermass, predict l.fluoro using the model
+extra.dat <- read.csv(file = "ctd_data.csv", header = T)
+extra.dat <- extra.dat[extra.dat$stn %in% c(3, 13, 23, 33, 43, 53, 63, 73, 83, 93), ] #use 10 stations that weren't used to fit the model
+
+pval <- 0
+se <- 0
+for(i in 1:nrow(extra.dat)) {
+
+  capture.output({
+    pred <- predict(asreml.fit, classify = "wm:ice:par:temp:z", levels = list("z" = extra.dat$profile.depth[i], "temp" = extra.dat$temp[i], "ice" = extra.dat$ice[i], "par" = extra.dat$par[i], "wm" = extra.dat$water_mass[i]))
+  }, file = tempfile())
+  pval[i] <- pred$predictions$pvals["predicted.value"]$predicted.value
+  se[i] <- pred$predictions$pvals["standard.error"]$standard.error
+    
+  if(i %% 100 == 0) print(i)
+  
+}  
+#write.csv(cbind(pval, se), "pred_with_correlation.csv", row.names = F)
+
+#compare predicted values to actual values
+plot(extra.dat$l.fluoro, pval, xlab = "observed", ylab = "predicted")
+title("Observed vs predicted for extra stations")
+
+lat.plot <- xyplot(extra.dat$l.fluoro + pval ~ extra.dat$profile.depth | extra.dat$stn, 
+                   outer = FALSE, type = "l", xlab = "depth (m)", ylab = "l.fluoro")
+update(lat.plot, par.settings = simpleTheme(lwd = c(2, 1), col = c("dodgerblue", "red")))
+title("Observed (blue) vs predicted (red) by station")
+
+
+#--------------------------- CHECK AUTOCORRELATION ----------------------------#
 
 #variogram of residuals for model with and without correlation strucutre
 d <- 62
@@ -118,17 +164,12 @@ plot(dist, gamma)
 
 #fit gamm to compare
 gamm.fit <- gamm(l.obs ~ s(z) + s(temp) + s(sal) + s(par) + s(oxy) + s(ice), random = list(stn =~ 1, x =~1, y =~1), 
-                 data = glm.spl)
+                 data = glm.spl, correlation = corAR1(0.9, z ~ 1))
 summary(gamm.fit$gam)
 
 
-#plot fitted against observed
-unique(glm.data$stn)
-s <-    14    
-plot(glm.data$profile.depth[glm.data$stn == s], glm.data$L.fluoro[glm.data$stn == s])
-points(glm.data$profile.depth[glm.data$stn == s], fitted(asreml.02)[glm.data$stn == s], col = "red")
-
-
-
+#plot fitted against observed by station
+lat.plot <- xyplot(glm.spl$l.obs + fitted(gamm.fit$gam) ~ glm.spl$z | glm.spl$stn, outer = FALSE, type = "l")
+update(lat.plot, par.settings = simpleTheme(lwd = c(2, 1), col = c("dodgerblue", "red")))
 
 
