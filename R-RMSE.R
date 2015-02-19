92fdcbd0f4d6319f4d1e8efb93eccece52c9ea04
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-02-20 10:26:40 +1100

    Added functionality to run all dropped arms automatically. Still need to add function to compare null and full model to check error model effect.

--- R-RMSE.R
+++ R-RMSE.R
@@ -1,13 +1,28 @@
 setwd(dir = "C:/Users/Lisa/Documents/phd/southern ocean/Mixed models/Data")
-dat <- read.csv("cross_val_drop_arm_45_59.csv", header = T)
+files <- list.files(pattern = "cross_val_drop_arm")
 
-RMSE_z <- sqrt((dat$predicted - dat$observed)^2)
-
-plot(dat$depth, RMSE_z, xlab = "z")
-
+RMSE_z <- NULL
+z <- NULL
+arm <- NULL
 RMSE_stn <- 0
-for (s in dat$stn) {
-  RMSE_stn[s]  <- sqrt(sum(na.omit((dat$predicted[dat$stn == s] - dat$observed[dat$stn == s])^2))/length(unique(dat$depth[dat$stn == s])))
+
+for (i in 1:length(files)) {
+  
+  dat <- read.csv(files[i], header = T)
+  
+  RMSE_z <- c(RMSE_z, sqrt((dat$predicted - dat$observed)^2))
+  z <- c(z, dat$depth)
+  arm <- c(arm, rep(i, nrow(dat)))
+    
+  for (s in dat$stn) {
+    RMSE_stn[s]  <- sqrt(sum(na.omit((dat$predicted[dat$stn == s] - dat$observed[dat$stn == s])^2))/length(unique(dat$depth[dat$stn == s])))
+  }
+  
 }
+
+plot(z, RMSE_z, xlab = "z")
+plot(RMSE_stn)
+
+
 
 
