d1f7251802ab90687916dec98dc3b3476736ed26
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-02-20 15:31:00 +1100

    No major changes.

--- R-RMSE.R
+++ R-RMSE.R
@@ -8,17 +8,22 @@ RMSE_stn <- 0
 
 for (i in 1:length(files)) {
   
+  #for each dropped arm, calculate RMSE
+  
   dat <- read.csv(files[i], header = T)
   
+  #RMSE by depth
   RMSE_z <- c(RMSE_z, sqrt((dat$predicted - dat$observed)^2))
   z <- c(z, dat$depth)
   arm <- c(arm, rep(i, nrow(dat)))
     
+  #RMSE by station
   for (s in dat$stn) {
     RMSE_stn[s]  <- sqrt(sum(na.omit((dat$predicted[dat$stn == s] - dat$observed[dat$stn == s])^2))/length(unique(dat$depth[dat$stn == s])))
   }
   
 }
+RMSE_stn[1] <- NA
 
 plot(z, RMSE_z, xlab = "z")
 plot(RMSE_stn)
