ec4878a0e2805e663013caf1ae5e86c612fc575a
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-03-05 12:40:32 +1100

    Creates fitted values by hand to check whether asreml includes random effects in its fitted values, which it does.

---
+++ R-test-fitted-inclusions.R
@@ -0,0 +1,19 @@
+asreml.fit <- asreml(fixed = l.obs ~ z, random =~ stn, 
+                     data = glm.spl, rcov=~ ar1(z.fact):agau(x.fact, y.fact),
+                     na.method.X = "include", workspace = 50000000)
+asreml.fit <- update(asreml.fit)
+
+
+s <- 10
+
+intercept <- asreml.fit$coefficients$fixed[2]
+z_fitted <- asreml.fit$coefficients$fixed[1]
+z <- glm.spl$z[glm.spl$stn == 2]
+stn_re <- asreml.fit$coefficients$random[s - 1]
+
+fit <- intercept + z_fitted*z + stn_re
+fit[is.na(asreml.fit$fitted[glm.spl$stn == s])] <- NA
+
+plot(asreml.fit$fitted[glm.spl$stn == s])
+points(fit, col = "red")
+
