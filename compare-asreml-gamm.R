6aeefbf72871fdf2cafbdeb3bbaefe61f3bf9caa
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-06-23 15:55:22 +1000

    No major changes

--- compare-asreml-gamm.R
+++ compare-asreml-gamm.R
@@ -90,8 +90,8 @@ glm.spl$oxy  <- scale(glm.spl$oxy)
 #------------------------------- FIT ASREML MODELS -----------------------------------#
 
 #fit asreml model
-asreml.fit <- asreml(fixed = l.obs ~ z + par + temp:wm + oxy, random =~ spl(z, 10) + spl(par, 10) + 
-                       spl(temp, 10):wm + spl(oxy, 10) + stn, 
+asreml.fit <- asreml(fixed = l.obs ~ z + par + temp:wm + sal + oxy, random =~ spl(z, 10) + spl(par, 10) + 
+                       spl(temp, 10):wm + spl(sal, 10) + spl(oxy, 10) + stn, 
                      data = glm.spl, rcov=~ ar1(z.fact):agau(x.fact, y.fact),
                      na.method.X = "include", workspace = 50000000)
 asreml.fit <- update(asreml.fit)
