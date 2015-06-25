d918dd4e29267dfe893cd1c385dfa285eea9f32a
Lisa-Marie Harrison <lisamarie.k.harrison@gmail.com>
2015-06-25 13:47:24 +1000

    Added summary statistics table

--- batch_test_run_3d_agau_irregular.R
+++ batch_test_run_3d_agau_irregular.R
@@ -10,12 +10,12 @@ z.phi    <- 0
 x.phi    <- 0
 y.phi    <- 0
 
-for (i in 1:200) {
+for (i in 1:10) {
   
-  glm.spl <- simData(noise.sd = 0.2, stn.sd = 0.05, z.phi = 0.35,
+  glm.spl <- simData(noise.sd = 0.45, stn.sd = 0.22, z.phi = 0.35,
                      x.phi = 0.5, y.phi = 0.4)
   
-  asreml.fit <- asreml(fixed = l.obs ~ z, random =~ spl(z) + stn, data = glm.spl, 
+  asreml.fit <- asreml(fixed = l.obs ~ z + par + temp, random =~ spl(z) + spl(par) + spl(temp) + stn, data = glm.spl, 
                     splinepoints = list(z = seq(0, 250, 25)), rcov=~ ar1(z.fact):agau(x, y))
   
 
@@ -49,13 +49,16 @@ abline(v = 0.4, col = "red", lwd = 2)
 abline(v = mean(y.phi), col = "blue", lwd = 2)
 
 
+stats_tab <- rbind(mean(stn.sd), mean(noise.sd), mean(z.phi), mean(x.phi), mean(y.phi))
+stats_tab <- cbind(stats_tab, c(sd(stn.sd), sd(noise.sd), sd(z.phi), sd(x.phi), sd(y.phi)))
+stats_tab <- cbind(stats_tab, c(min(stn.sd), min(noise.sd), min(z.phi), min(x.phi), min(y.phi)))
+stats_tab <- cbind(stats_tab, c(max(stn.sd), max(noise.sd), max(z.phi), max(x.phi), max(y.phi)))
+stats_tab <- cbind(stats_tab,  c(quantile(stn.sd)[2], quantile(noise.sd)[2], quantile(z.phi)[2], quantile(x.phi)[2], quantile(y.phi)[2]))
+stats_tab <- cbind(stats_tab,  c(quantile(stn.sd)[4], quantile(noise.sd)[4], quantile(z.phi)[4], quantile(x.phi)[4], quantile(y.phi)[4]))
+rownames(stats_tab) <- c("stn.sd", "noise.sd", "depth", "lat", "long")
+colnames(stats_tab) <- c("mean", "sd", "min", "max", "25% quantile", "75% quantile")
+stats_tab
 
-
-mean(stn.sd)
-mean(noise.sd)
-mean(z.phi)
-mean(x.phi)
-mean(y.phi)
 
 
 
