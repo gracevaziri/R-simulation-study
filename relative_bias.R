dab205588238fd96b0020ed4ff6cce41e9a0ed25
Lisa-Marie Harrison uni <lisamarie.k.harrison@gmail.com>
2016-08-25 12:17:33 +1000

    Added function to calculate 95% confidence intervals

--- relative_bias.R
+++ relative_bias.R
@@ -1,4 +1,4 @@
-dat <- read.csv("C:/Users/43439535/Documents/Lisa/phd/Mixed models/Data/3d_ar1_agau_irregular_grid_estimates.csv", header = T)
+dat <- read.csv("C:/Users/43439535/Documents/Lisa/phd/Mixed models/Data/pars_new.csv", header = T)
 
 
 #true input values
@@ -6,12 +6,23 @@ par_vals <- c("stn.sd" = 0.22, "noise.sd" = 0.45, "z.phi" = 0.35,
               "x.phi" = 0.5, "y.phi" = 0.4)
 
 #estimator means
-est_mean <- apply(dat, 2, mean)
+est_mean <- apply(dat[, 1:5], 2, mean)
 
 #relative bias
 est_mean - par_vals
 
 #confidence intervals
 
+calcCI <- function (x) {
+  
+  lower <- mean(x) - 1.96*(sd(x)/sqrt(length(x)))
+  upper <- mean(x) + 1.96*(sd(x)/sqrt(length(x)))
+  
+  return(c(lower, upper))
+  
+}
+
+apply(dat[, 1:5], 2, calcCI)
 
 #coverage probabilities
+
